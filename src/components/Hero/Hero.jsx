8f77bbbac73d68bb0af3e8e9f6a97296993ee7f3
Nikita Shah <ns079287@gmail.com>
2026-02-06 21:24:25 +0530

    hero page update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <div id='home'className='hero'>
         <img src={profile_img} alt="" />
-        <h1> <span>I'm Nikita shah, </span>Frontend developer based in INDIA.</h1>
+        <h1> <span>I'm Nikita shah, </span>Full Stack developer based in INDIA.</h1>
         <p>Full Stack Developer | Passionate about building scalable web apps with clean design and modern technologies. </p>
         <div className="hero-action">
         <div className='hero-connect'><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink> </div>
